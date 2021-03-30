f8d914e09d9f2718b1a704b81bb5cef1a93f4057
rafael <rafaelfilipeavelar@hotmail.com>
2021-03-29 21:20:21 -0300

    ex022.py

--- ex011.py
+++ ex011.py
@@ -1,7 +1,7 @@
-largura = float(input('Digite a largura dessa parede em m:'))
-altura = float(input('Digite a altura da parede em m:'))
+largura = float(input('Digite a largura dessa parede em m: '))
+altura = float(input('Digite a altura da parede em m: '))
 a = largura * altura
-tinta = a / 2
+tinta = a/2
 print('Essa parede tem a dimensão de {}X{} e sua area sera {:.2f}m2'
       ' voce precisará de {}L de tinta'.format(largura, altura, a, tinta))
 
